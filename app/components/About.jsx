c508044393414af82ed7f2e628fc003bfdb0f6ec
Umer <umerjamil@protonmail.com>
2017-06-20 21:32:16 +0500

    Changes in About comp.

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -14,7 +14,11 @@ var ReactDOM = require("react-dom");
 
 var About = (props) =>{
     return(
-        <h3>About Component</h3>
+        <div>
+        <h3>About </h3>
+        <p>Welcome to the About page</p>
+        </div>
+
     );
 }
 
