fccd9cf98fd1d213cdbd4e42fc66d5fb5b28a4cc
Umer <umerjamil@protonmail.com>
2017-06-23 21:27:04 +0500

    Use SASS

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
       WeatherMessage: "app/components/WeatherMessage.jsx",
       OpenWeatherMap: "app/API/OpenWeatherMap.jsx",
       ErrorModal: "app/components/ErrorModal.jsx",
-      applicationStyles: "app/styles/app.css"
+      applicationStyles: "app/styles/app.scss"
     },
     extensions: ['', '.js', '.jsx']
   },
