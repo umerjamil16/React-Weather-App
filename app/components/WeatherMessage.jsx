5930105764873e1f18267622a15d2681e8583f1e
Umer <umerjamil@protonmail.com>
2017-06-23 23:04:39 +0500

    Add nav search Support

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -16,7 +16,7 @@ var ReactDOM = require("react-dom");
 var WeatherMessage = (props)=>{
     return (
             <div>
-            <h3 className="text-center">Its {props.temp} in {props.location}</h3>
+            <h3 className="text-center">Its {props.temp}°F in {props.location}</h3>
             </div>
         );
 }
